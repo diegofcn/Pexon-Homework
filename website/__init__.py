0cb436004e99a58c8b681b338fb879e5705949c3
diegofcn <94481027+diegofcn@users.noreply.github.com>
2022-09-11 14:09:13 +0200

    login
    Created a test user and made the login work

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
-from .views import views
-from .auth import auth
+from flask_login import LoginManager
 
 
 
@@ -16,12 +15,23 @@ def create_app():
 
     db.init_app(app)
 
+    from .views import views
+    from .auth import auth
+
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
 
     from .models import User, Certificate
 
     create_database(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
     
     return app
 
