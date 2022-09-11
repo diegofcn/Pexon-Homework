041c15b5cdc30b43af8585b35f6c9339476ea867
diegofcn <94481027+diegofcn@users.noreply.github.com>
2022-09-11 13:39:12 +0200

    Set up Flask sqlalchemy and created a Database with sqlite3

---
+++ website/models.py
@@ -0,0 +1,17 @@
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+class Certificate(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(5000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(50))
+    first_name = db.Column(db.String(50))
+    last_name = db.Column(db.String(50))
+    certificates = db.relationship('Certificate')
