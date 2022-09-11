369d3d6aefeceae71ce9ecd0ccf412e62c205c94
diegofcn <94481027+diegofcn@users.noreply.github.com>
2022-09-11 14:39:40 +0200

    adding certificates
    Certificates can now be added and will be displayed on the screen

--- website/views.py
+++ website/views.py
@@ -1,9 +1,20 @@
-from flask import Blueprint, render_template
+from unicodedata import category
+from flask import Blueprint, render_template, request, flash
 from flask_login import login_required, current_user
+from .models import Certificate
+from . import db
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
+@views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
+    if request.method == 'POST':
+        certificate = request.form.get('certificate')
+        if len(certificate) < 1:
+            flash('Text is too short!', category='error')
+        else:
+            new_certificate = Certificate(title=certificate, user_id=current_user.id)
+            db.session.add(new_certificate)
+            db.session.commit()
     return render_template("home.html", user=current_user)
