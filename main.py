287602f1c403c90d0b278287c4e8b1bcce4ad422
diegofcn <94481027+diegofcn@users.noreply.github.com>
2022-09-12 23:31:47 +0200

    deleted hardcoded port

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from website import create_app
+import os
 
 app = create_app()
 
 # only if we run this file, app.run will be executed
 if __name__ == '__main__':
-    app.run(debug=True, port=8000, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0", port=os.getenv('PORT'))
